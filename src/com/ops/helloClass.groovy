6cbf6022590d7ed1e5a23296d194cafef5d0479c
ithank <henry.happ@gmail.com>
2023-08-25 16:30:24 -0500

    added map" "

--- src/com/ops/helloClass.groovy
+++ src/com/ops/helloClass.groovy
@@ -4,7 +4,8 @@ public class helloClass {
     def whatIsForDinner(Map config) {
       sh "echo Dinner is ${config.meal}."
     }
-    def nope(){
+
+    def nope(Map config) {
       sh "echo NOPE!"
     }
 }
