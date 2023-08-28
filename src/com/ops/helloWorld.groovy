f23bddfdad6c882a900f80e631a86abdc9afdcf6
Henry Happ <ithank@users.noreply.github.com>
2023-08-28 07:44:41 -0500

    Update helloWorld.groovy

--- src/com/ops/helloWorld.groovy
+++ src/com/ops/helloWorld.groovy
@@ -1,9 +1,9 @@
-def call(Map config) {
+def call(Map config = [:]) {
     // you can call any valid step functions from your code, just like you can from Pipeline scripts
     sh "echo Hello world, ${config.name}"
 }
 
-def louder(Map config) {
+def louder(Map config = [:]) {
     def msg = "Hello world, ${config.name}"
     msg = msg.toUpperCase()
     sh "echo ${msg}"
