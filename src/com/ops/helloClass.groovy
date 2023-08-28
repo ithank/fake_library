cb6c6a66529227f4d727eeba10ad4f42397c84ff
ithank <henry.happ@gmail.com>
2023-08-28 07:08:00 -0500

    nope

--- src/com/ops/helloClass.groovy
+++ src/com/ops/helloClass.groovy
@@ -1,9 +1,9 @@
 package com.ops
 
 public class helloClass {
-    def whatIsForDinner(Map config) {
-      sh "echo Dinner is ${config.meal}."
-    }
+    // def whatIsForDinner(Map config) {
+    //   sh "echo Dinner is ${config.meal}."
+    // }
 
     def nope(Map config) {
       sh "echo NOPE!"
