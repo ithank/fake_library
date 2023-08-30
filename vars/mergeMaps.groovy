6e9e8b9286358bcfc3c06c40b317d9047203a937
Henry Happ <ithank@users.noreply.github.com>
2023-08-30 08:30:22 -0500

    Create mergeMaps.groovy

---
+++ vars/mergeMaps.groovy
@@ -0,0 +1,18 @@
+def call(Map... maps) {
+    Map result
+
+    if (maps.length == 0) {
+        result = [:]
+    } else if (maps.length == 1) {
+        result = maps[0]
+    } else {
+        result = [:]
+        maps.each { map ->
+            map.each { k, v ->
+                result[k] = result[k] instanceof Map ? merge(result[k], v) : v
+            }
+        }
+    }
+
+    result
+}
