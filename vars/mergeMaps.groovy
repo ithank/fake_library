ee0b6231611730bea237becb8868e036043ee80a
Henry Happ <ithank@users.noreply.github.com>
2023-08-30 09:02:21 -0500

    Update mergeMaps.groovy

--- vars/mergeMaps.groovy
+++ vars/mergeMaps.groovy
@@ -1,18 +1,6 @@
-def call(Map... maps) {
-    Map result
+import com.ops
 
-    if (maps.length == 0) {
-        result = [:]
-    } else if (maps.length == 1) {
-        result = maps[0]
-    } else {
-        result = [:]
-        maps.each { map ->
-            map.each { k, v ->
-                result[k] = result[k] instanceof Map ? merge(result[k], v) : v
-            }
-        }
-    }
-
-    result
+def call(Map... maps){
+    def mm =  new mapMerge(maps)
+    return mm.merge(maps)
 }
