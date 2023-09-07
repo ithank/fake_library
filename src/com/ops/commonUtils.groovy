8be111d96d378a3f165c9c45f329cf3c63e01584
Henry Happ <ithank@users.noreply.github.com>
2023-09-07 14:26:25 -0500

    Update commonUtils.groovy

--- src/com/ops/commonUtils.groovy
+++ src/com/ops/commonUtils.groovy
@@ -62,8 +62,11 @@ public class commonUtils
   {
     context.wrap([$class: 'BuildUser'])
     {
-      context.println "Retrieving User AD Groups for user: ${context.env.BUILD_USER_ID}"
-      def auths = Jenkins.instance.securityRealm.loadUserByUsername(context.env.BUILD_USER_ID)
+      //context.println "Retrieving User AD Groups for user: ${context.env.BUILD_USER_ID}"
+      //def auths = Jenkins.instance.securityRealm.loadUserByUsername(context.env.BUILD_USER_ID)
+      //  .authorities.collect{a -> a.authority}
+      context.println "Retrieving User AD Groups for user: test_user"
+      def auths = Jenkins.instance.securityRealm.loadUserByUsername(test_user)
       .authorities.collect{a -> a.authority}
 
       context.println "AD Groups: ${auths}"
