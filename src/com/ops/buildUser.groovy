f4ed3983c136dd0294c852a2fb65be50e9ce7897
Henry Happ <ithank@users.noreply.github.com>
2023-09-07 07:22:42 -0500

    Create buildUser.groovy

---
+++ src/com/ops/buildUser.groovy
@@ -0,0 +1,20 @@
+import com.ops
+
+public class buildUser
+{
+  def context
+  public buildUser(context)
+  {
+    this.context = context
+  }
+
+  public Map getBuildUser()
+  {
+    def user = [:]
+
+    user.name = context.BUILD_USER
+    user.email = context.BUILD_USER_EMAIL
+    return user
+  }
+  
+}
