39a7c69e33588611230458e83ad14642edc73115
Henry Happ <ithank@users.noreply.github.com>
2023-09-07 11:21:47 -0500

    Create commonUtils.groovy

---
+++ src/com/ops/commonUtils.groovy
@@ -0,0 +1,39 @@
+package com.ops
+
+
+  @NonCPS
+  public sortVersionList(list)
+  {
+    def versionComparator = { a, b ->
+      def VALID_TOKENS = /._/
+      a = a.tokenize(VALID_TOKENS)
+      b = b.tokenize(VALID_TOKENS)
+
+      for (i in 0..<Math.max(a.size(), b.size())) {
+        if (i == a.size()) {
+          return b[i].isInteger() ? -1 : 1
+        } else if (i == b.size()) {
+          return a[i].isInteger() ? 1 : -1
+        }
+
+        if (a[i].isInteger() && b[i].isInteger()) {
+          int c = (a[i] as int) <=> (b[i] as int)
+          if (c != 0) {
+            return c
+          }
+        } else if (a[i].isInteger()) {
+          return 1
+        } else if (b[i].isInteger()) {
+          return -1
+        } else {
+          int c = a[i] <=> b[i]
+          if (c != 0) {
+            return c
+          }
+        }
+      }
+      return 0
+    }
+
+    return list.sort(versionComparator)
+  }
